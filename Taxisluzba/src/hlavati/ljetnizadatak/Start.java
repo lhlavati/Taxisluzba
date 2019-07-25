7d6d3481e197621220331f9c1f41e7d72179fd60
lhlavati <lukahlavati@gmail.com>
2019-07-25 20:26:48 +0200

    Stavljanje case-a 1 i 2 u metode zbog preglednosti

--- Taxisluzba/src/hlavati/ljetnizadatak/Start.java
+++ Taxisluzba/src/hlavati/ljetnizadatak/Start.java
@@ -10,44 +10,66 @@ import java.sql.PreparedStatement;
 public class Start {
 
 	private Connection veza;
-	PreparedStatement izraz;
+	private PreparedStatement izraz;
+	Desktop d = Desktop.getDesktop();
 	
 	public Start() {
 		
+		veza = Baza.getConnection();
+		
 		izlaz:
 		while(true) {
-			Desktop d = Desktop.getDesktop();
+			
 			izbornik();
 			switch (Integer.parseInt(JOptionPane.showInputDialog("Unesite redni broj stavke:"))) {
 			case 0: // IZLAZ
 				JOptionPane.showMessageDialog(null, "Doviðenja!");
 				break izlaz;
 			case 1: // GIT URL ERA dijagrama
-				
-				try {
-					d.browse(new URI("https://github.com/lhlavati/Taxisluzba/blob/master/ERA/ERA%20taxisluzba.png"));
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-				JOptionPane.showMessageDialog(null, "URL uspješno otvoren!");
+				urlEra();
 				break;
-				
 			case 2: // URL GITHUB KODA
-				
-				try {
-					d.browse(new URI("https://github.com/lhlavati/Taxisluzba/blob/master/Taxisluzba/src/hlavati/ljetnizadatak/Start.java"));
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-				JOptionPane.showMessageDialog(null, "URL uspješno otvoren!");
+				urlGit();
 				break;
+			case 3: // èitanje
 				
-			case 3:
+				break;
+			case 4: // unos
+				
+				break;
+			case 5: // promjena
+				
+				break;
+			case 6: // brisanje
+				
+				break;
 			default:
 				break;
 			}
 			
 		}
+		
+	}
+	
+	private void urlGit() {
+		
+		try {
+			d.browse(new URI("https://github.com/lhlavati/Taxisluzba/blob/master/Taxisluzba/src/hlavati/ljetnizadatak/Start.java"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		JOptionPane.showMessageDialog(null, "URL uspješno otvoren!");
+		
+	}
+
+	private void urlEra() {
+		
+		try {
+			d.browse(new URI("https://github.com/lhlavati/Taxisluzba/blob/master/ERA/ERA%20taxisluzba.png"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		JOptionPane.showMessageDialog(null, "URL uspješno otvoren!");
 		
 	}
 	
@@ -58,7 +80,7 @@ public class Start {
 		System.out.println("##  3. Èitanje svih podataka odabrane tablice ##");
 		System.out.println("##  4. Unos svih podataka u odabranu tablicu  ##");
 		System.out.println("##  5. Promjena podataka u odabranoj tablici  ##");
-		System.out.println("##  4. Brisanje podataka u odabranoj tablici  ##");
+		System.out.println("##  6. Brisanje podataka u odabranoj tablici  ##");
 		System.out.println("##  0. Izlaz				      ##");
 		System.out.println("################################################");
 	}
