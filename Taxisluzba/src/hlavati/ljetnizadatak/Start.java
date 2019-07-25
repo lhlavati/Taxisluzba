bc8cd3987e810e489b344b07caa11be6a6a1ad02
lhlavati <lukahlavati@gmail.com>
2019-07-25 19:23:59 +0200

    Realizirana stavka 7, (u izborniku postaviti stavku koja otvara github kod)

--- Taxisluzba/src/hlavati/ljetnizadatak/Start.java
+++ Taxisluzba/src/hlavati/ljetnizadatak/Start.java
@@ -10,11 +10,14 @@ public class Start {
 	public Start() {
 		izlaz:
 		while(true) {
+			Desktop d = Desktop.getDesktop();
+			izbornik();
 			switch (Integer.parseInt(JOptionPane.showInputDialog("Unesite redni broj stavke:"))) {
 			case 0: // IZLAZ
+				JOptionPane.showMessageDialog(null, "Doviðenja!");
 				break izlaz;
 			case 1: // GIT URL ERA dijagrama
-				Desktop d = Desktop.getDesktop();
+				
 				try {
 					d.browse(new URI("https://github.com/lhlavati/Taxisluzba/blob/master/ERA/ERA%20taxisluzba.png"));
 				} catch (Exception e) {
@@ -22,16 +25,30 @@ public class Start {
 				}
 				JOptionPane.showMessageDialog(null, "URL uspješno otvoren!");
 				break;
-
+				
+			case 2: // URL GITHUB KODA
+				
+				try {
+					d.browse(new URI("https://github.com/lhlavati/Taxisluzba/blob/master/Taxisluzba/src/hlavati/ljetnizadatak/Start.java"));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				JOptionPane.showMessageDialog(null, "URL uspješno otvoren!");
+				break;
 			default:
 				break;
 			}
 			
 		}
 		
-		
 	}
 	
+	private void izbornik() {
+		
+	}
+
+
+
 	public static void main(String[] args) {
 		new Start();
 	}
