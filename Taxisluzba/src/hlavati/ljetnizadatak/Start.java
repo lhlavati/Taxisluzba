1f4c86c96c6a88c0bbff4b52d1da50e7f1365b21
lhlavati <lukahlavati@gmail.com>
2019-07-26 14:49:27 +0200

    Dodan Insert i Select

--- Taxisluzba/src/hlavati/ljetnizadatak/Start.java
+++ Taxisluzba/src/hlavati/ljetnizadatak/Start.java
@@ -9,6 +9,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.Date;
 
 public class Start {
 
@@ -38,7 +39,7 @@ public class Start {
 				citajIzBaze();
 				break;
 			case 4: // unos
-				
+				unesiUBazu();
 				break;
 			case 5: // promjena
 				
@@ -54,9 +55,52 @@ public class Start {
 		
 	}
 	
+	private void unesiUBazu() {
+		
+		System.out.println("1. vozac\n2. vozi\n3. vozilo\n4. voznja\n5. IZLAZ");
+		switch (Integer.parseInt(JOptionPane.showInputDialog("Unesite tablicu u koju biste htjeli unjeti nove podatke"))) {
+		case 1:
+			
+			try {
+				izraz = veza.prepareStatement("INSERT INTO vozilo (marka, gorivo, snaga, ABS_, godiste, brojVozila) VALUES (?, ?, ?, ?, ?, ?)");
+				izraz.setString(1, JOptionPane.showInputDialog("Unesite marku vozila"));
+				izraz.setString(2, JOptionPane.showInputDialog("Unesite vrstu goriva"));
+				izraz.setString(3, JOptionPane.showInputDialog("Unesite snagu motora (npr: '66 kW'"));
+				izraz.setBoolean(4, Boolean.parseBoolean(JOptionPane.showInputDialog("Unesite ima li vaše vozilo ABS (true ili false)")));
+				izraz.setInt(5, Integer.parseInt(JOptionPane.showInputDialog("Unesite godiste vozila")));
+				izraz.setInt(6, Integer.parseInt(JOptionPane.showInputDialog("Unesite broj vozila koje želite unjeti")));
+				System.out.println("Uneseno je " + izraz.executeUpdate());
+			    
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			break;
+		case 2:
+			
+			try {
+				
+				ispisiTablicu("SELECT * FROM vozilo");
+				ispisiTablicu("SELECT * FROM vozac");
+				izraz = veza.prepareStatement("INSERT INTO vozi (vozilo, vozac) VALUES (?, ?)");
+				izraz.setInt(1, Integer.parseInt(JOptionPane.showInputDialog("Unesite sifru vozila")));
+				izraz.setInt(2, Integer.parseInt(JOptionPane.showInputDialog("Unesite sifru vozaca")));
+				JOptionPane.showMessageDialog(null, ("Uspješno uneseno (" + izraz.executeUpdate() + ")"));
+			    
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			break;
+		default:
+			break;
+		}
+		
+	}
+
 	private void citajIzBaze() {
 		
-		System.out.println("1. vozac\n2. vozilo\n3. vozi\n4. voznja\n5. izlaz");
+		System.out.println("1. vozac\n2. vozilo\n3. vozi\n4. voznja\n5. IZLAZ");
 		switch (Integer.parseInt(JOptionPane.showInputDialog("Unesite redni broj tablice koju biste htjeli èitati"))) {
 			case 1:
 				ispisiTablicu("SELECT * FROM vozac");
@@ -108,7 +152,7 @@ public class Start {
 
 	private void urlGit() {
 		
-		System.out.println("1. Start.java (highly recommended)\n2. Baza.java\n3. Izlaz");
+		System.out.println("1. Start.java (highly recommended)\n2. Baza.java\n3. IZLAZ");
 		try {
 			switch (Integer.parseInt(JOptionPane.showInputDialog("Unesite redni broj klase koju zelite otvoriti"))) {
 			case 1:
