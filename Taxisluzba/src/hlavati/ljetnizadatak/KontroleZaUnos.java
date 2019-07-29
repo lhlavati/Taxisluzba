8595942d1a08385b10d116aaa49069169d4a0ef8
lhlavati <50337055+lhlavati@users.noreply.github.com>
2019-07-29 16:27:27 +0200

    Boolean metoda provjere ABS-a

--- Taxisluzba/src/hlavati/ljetnizadatak/KontroleZaUnos.java
+++ Taxisluzba/src/hlavati/ljetnizadatak/KontroleZaUnos.java
@@ -5,28 +5,27 @@ import javax.swing.JOptionPane;
 public class KontroleZaUnos {
 
 	public static String unosString(String poruka) {
-		
+
 		String s;
-		while(true) {
-			
-			s=JOptionPane.showInputDialog(poruka);
-			if(s.trim().length()==0) {
-				JOptionPane.showMessageDialog(null,"Obavezan unos");
+		while (true) {
+
+			s = JOptionPane.showInputDialog(poruka);
+			if (s.trim().length() == 0) {
+				JOptionPane.showMessageDialog(null, "Obavezan unos");
 				continue;
 			}
 			return s;
 		}
-		
+
 	}
-	
-	
+
 	public static int unosInt(String poruka) {
-		
+
 		int i;
-		while(true) {
+		while (true) {
 			try {
-				i=Integer.parseInt(JOptionPane.showInputDialog(poruka));
-				if(i<=0) {
+				i = Integer.parseInt(JOptionPane.showInputDialog(poruka));
+				if (i <= 0) {
 					JOptionPane.showMessageDialog(null, "Broj mora biti veæi od nule");
 					continue;
 				}
@@ -35,9 +34,25 @@ public class KontroleZaUnos {
 				JOptionPane.showMessageDialog(null, "Obavezan unos broja");
 			}
 		}
-		
+
 	}
-	
-	
-	
+
+	public static Boolean unosBoolean(String poruka) {
+		boolean DA = true;
+		boolean NE = false;
+		boolean b;
+		while (true) {
+
+			try {
+				b = Boolean.parseBoolean(JOptionPane.showInputDialog(poruka));
+				if (b == DA || b == NE) {
+					return b;
+				}
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(null, "Obavezan unos");
+			}
+
+		}
+	}
+
 }
